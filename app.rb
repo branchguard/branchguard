0ac09869b559acbab731df2f03a168338bd09c7f
Brandon Valentine <brandon@brandonvalentine.com>
2019-08-22 20:51:31 -0500

    Make first test pass

--- app.rb
+++ app.rb
@@ -12,6 +12,7 @@ post '/payload' do
 end
 
 def verify_signature(payload_body)
+  return halt 401 unless request.env['HTTP_X_HUB_SIGNATURE']
   signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
-  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+  return halt 401, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
 end
