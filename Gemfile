5a48629ead50d0b8e131548ae6e6f665eebde248
Brandon Valentine <brandon@brandonvalentine.com>
2019-08-22 19:57:33 -0500

    Verify signature of incoming webhook

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem "sinatra", "~> 2.0"
 gem "thin", "~> 1.7"
 gem "json", "~> 2.2"
+gem "dotenv", "~> 2.7"
--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
+require 'dotenv/load'
 require 'sinatra'
 require 'json'
+require 'rack/utils'
 
 post '/payload' do
+  payload_body = request.body.read
+  verify_signature(payload_body)
+  request.body.rewind
   push = JSON.parse(request.body.read)
   puts "I got some JSON: #{push.inspect}"
+end
+
+def verify_signature(payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
 end
